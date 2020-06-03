7e637f147c5e3bbd6f6be9e3329e22e215ef91d5
Miles Budden <miles@pop-os.localdomain>
2020-06-03 19:16:40 +0100

    Init

---
+++ slow_django/middleware.py
@@ -0,0 +1,17 @@
+import time
+
+def slowdown(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+        time.sleep(5)
+        return response
+
+    return middleware
---
+++ tests/test_slow_django.py
@@ -0,0 +1,5 @@
+from slow_django import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
